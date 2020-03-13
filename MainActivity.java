448c0ebe414bd0e8b1637a348658f4c8e81e17a2
Rachanagangadhar <43091117+Rachanagangadhar@users.noreply.github.com>
2020-03-13 10:02:11 +0530

    program 4

--- MainActivity.java
+++ MainActivity.java
@@ -1,63 +1,36 @@
-package com.example.intents;
+package com.example.pro4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.EditText;
-
-import android.widget.Spinner;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText e1,e2;
 
-    Button bt;
-
-    Spinner s;
-    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
-
-    String name,reg,dept;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1= (EditText) findViewById(R.id.editText);
+        Bitmap bg=Bitmap.createBitmap(720,1280,Bitmap.Config.ARGB_8888);
+        ImageView i=(ImageView)findViewById(R.id.imageView);
+        i.setBackgroundDrawable(new BitmapDrawable(bg));
+        Canvas canvas=new Canvas(bg);
+        Paint paint=new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setTextSize(50);
+        canvas.drawText("Circle", 120, 150, paint);
 
-        e2= (EditText) findViewById(R.id.editText2);
-
-        bt= (Button) findViewById(R.id.button);
-
-        s= (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
-
-        s.setAdapter(adapter);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name=e1.getText().toString();
-
-                reg=e2.getText().toString();
-
-                dept=s.getSelectedItem().toString();
-
-//Intent For Navigating to Second Activity
-
-                Intent i = new Intent(MainActivity.this,secactivity.class);
-
-//For Passing the Values to Second Activity
-
-                i.putExtra("name_key", name);
-
-                i.putExtra("reg_key",reg);
-
-                i.putExtra("dept_key", dept);
-
-                startActivity(i);
-            }
-        });
+        canvas.drawCircle(200, 350, 150, paint);
+        canvas.drawText("Rectangle",420,150,paint);
+        canvas.drawRect(400,200,650,700,paint);
+        canvas.drawText("Square",120,800,paint);
+        canvas.drawRect(50,850,350,1150,paint);
+        canvas.drawText("Line",480,800,paint);
+        canvas.drawLine(520,850,520,1150,paint);
     }
 }
